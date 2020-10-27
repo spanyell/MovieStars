354e3b11408cc86233b24378367b078c3b849d45
Dan Beers <spanyell@icloud.com>
2020-10-27 13:49:41 -0700

    Initial Commit

---
+++ MovieStars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MovieStars
+//
+//  Created by Dan Beers on 10/27/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MovieStars/MovieStarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieStarsApp.swift
+//  MovieStars
+//
+//  Created by Dan Beers on 10/27/20.
+//
+
+import SwiftUI
+
+@main
+struct MovieStarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
